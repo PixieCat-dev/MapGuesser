83f6d409ef5879cd73cd643084b03d8325fb9dce
PixieCat-dev <115374158+PixieCat-dev@users.noreply.github.com>
2023-10-13 16:53:32 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,70 @@
+const mapDatabase = [{
+        link: 'https://s24.postimg.cc/gf4kvhrb9/europe_1900.png',
+        Time: ['world war I', 'world war 1', 'ww1', '1918'],
+    },
+    {
+        link: 'https://tile.loc.gov/image-services/iiif/service:gmd:gmd5:g5700:g5700:ct001356/full/pct:25/0/default.jpg',
+        Time: ['21st century', '2006'],
+    },
+    {
+        link: 'https://omniatlas-1598b.kxcdn.com/media/img/articles/subst/europe/europe19910906.png',
+        Time: ['1991', 'soviet collapse'],
+    },
+    {
+        link: 'https://omniatlas-1598b.kxcdn.com/media/img/articles/complete/europe/europe18120316.jpg',
+        Time: ['napoleonic wars', '1812', 'XIX', 'Napoleonic Wars'],
+    },
+];
+
+let currentMap;
+
+function getRandomMap() {
+    const randomIndex = Math.floor(Math.random() * mapDatabase.length);
+    return mapDatabase[randomIndex];
+}
+
+function loadNewMap() {
+    currentMap = getRandomMap();
+    const mapImage = document.getElementById('map-image');
+    mapImage.src = currentMap.link;
+    mapImage.alt = `Map from the ${currentMap.Time}`;
+    document.getElementById('result').textContent = '';
+}
+
+window.onload = function() {
+    loadNewMap();
+    document.getElementById('guess-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        checkGuess();
+        loadNewMap();
+    });
+};
+
+function checkGuess() {
+    const userGuess = document.getElementById('Time-guess').value.toLowerCase();
+    const correctAnswers = Array.isArray(currentMap.Time) ? currentMap.Time.map(answer => answer.toLowerCase()) : [currentMap.Time.toLowerCase()];
+
+    if (correctAnswers.includes(userGuess)) {
+        document.getElementById('correct-message').style.display = 'block';
+        setTimeout(() => {
+            loadNewMap();
+            document.getElementById('correct-message').style.display = 'none';
+        }, 2000);
+    } else {
+        document.getElementById('wrong-message').style.display = 'block';
+        setTimeout(() => {
+            loadNewMap();
+            document.getElementById('wrong-message').style.display = 'none';
+        }, 2000);
+    }
+
+    document.getElementById('submit-button').disabled = true;
+
+    document.getElementById('Time-guess').value = '';
+}
+
+const helpButton = document.getElementById('help-button');
+
+helpButton.addEventListener('click', function() {
+    window.location.href = 'help.html';
+});
